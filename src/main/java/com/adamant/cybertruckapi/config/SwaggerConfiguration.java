52ad7e74da19868c84461a2ad8b1bd330b8c1519
Daham <dahampositha33@gmail.com>
2024-03-23 10:56:26 +0530

    Update SwaggerConfiguration.java and openapi.yaml

--- src/main/java/com/adamant/cybertruckapi/config/SwaggerConfiguration.java
+++ src/main/java/com/adamant/cybertruckapi/config/SwaggerConfiguration.java
@@ -1,12 +1,9 @@
 package com.adamant.cybertruckapi.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
-import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,24 +22,11 @@ public class SwaggerConfiguration {
   public OpenAPI customOpenApi() {
     return new OpenAPI()
         .info(new Info()
-            .title("Cybertruck API")
-            .description("Cybertruck API")
-            .contact(new Contact()
-                .name("Daham")
-                .email("dahampositha33@gmail.com")
-                .url(""))
-            .extensions(Map.of(
-                "x-api-id", "location-api"
-            ))
+            .title("cybertruck-api")
             .version("1.0.0")
-            .license(
-                new License()
-                    .name("Apache 2.0")
-                    .url("http://www.apache.org/licenses/LICENSE-2.0"))
         )
         .servers(List.of(
             new Server()
-                .url("/")
-                .description("Local Server")));
+                .url("http://localhost:8080")));
   }
 }
